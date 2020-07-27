37758a0622fe6f6c306a2a9ba9586bcaac81786b
Yousuf Uyghur <yousufuighur@yahoo.com>
2020-07-27 14:07:12 +0500

    fixed bugs

--- src/CounterReducer.js
+++ src/CounterReducer.js
@@ -2,6 +2,9 @@ const CounterReducer = (state, action) => {
     switch (action) {
         case 'INCREMENT':
             return state + 1 
+
+        default:
+            console.log("What we know is a drop, what we dont know is an ocean."); 
     }
 }
 
